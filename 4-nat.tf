56f734bbc0fc85a7b3c9e58bc5a4d6194f271c9d
tiqsclass6 <daquietstorm22@gmail.com>
2025-11-16 13:29:43 -0800

    initial commit

---
+++ 4-nat.tf
@@ -0,0 +1,20 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.tiqs-igw]
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "tiqs-nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.tiqs-public-1.id
+
+  tags = {
+    Name = "tiqs"
+  }
+
+  depends_on = [aws_internet_gateway.tiqs-igw]
+}
---
+++ B-output.tf
@@ -0,0 +1,12 @@
+output "ip_address" {
+  value = aws_instance.tiqs-ec2-instance.public_ip
+}
+
+output "website_url" {
+  value = "http://${aws_instance.tiqs-ec2-instance.public_dns}"
+}
+
+# output "lb_dns_name" {
+#   value       = "http://${aws_lb.tiqs-lb.dns_name}"
+#   description = "The DNS name of the Auto Scale Group 01 - Load Balancer."
+# }
