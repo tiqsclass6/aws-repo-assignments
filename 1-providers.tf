d2d33fa1ffd3056e04400c094566a4d654306548
tiqsclass6 <daquietstorm22@gmail.com>
2025-12-03 22:24:48 -0800

    second commit

--- 1-providers.tf
+++ 1-providers.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.19.0"
-    }
-  }
-
-  backend "s3" {
-    bucket  = "class-7-state-files"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
